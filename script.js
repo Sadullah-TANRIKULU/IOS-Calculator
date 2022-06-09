610448d9ae04f8bc5eae5f41920b1f74b8e2f72c
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-06-09 06:55:27 +0300

    not ready yet but one day will be

--- script.js
+++ script.js
@@ -1,39 +1,75 @@
-let screen = document.querySelector(`.screen`);
-
 const container = document.querySelector(`.container`);
 const buttons = document.querySelector('.buttons');
+const up = document.querySelector('.up');
+const resultDown = document.querySelector('.down');
 
-let action;
+const calculate = (n1, operator, n2) => {
+    let result = '';
+    
+    if (operator === 'add') {
+      result = parseFloat(n1) + parseFloat(n2);
+    } else if (operator === 'subtract') {
+      result = parseFloat(n1) - parseFloat(n2);
+    } else if (operator === 'multiply') {
+      result = parseFloat(n1) * parseFloat(n2);
+    } else if (operator === 'divide') {
+      result = parseFloat(n1) / parseFloat(n2);
+    }
+    resultDown.textContent = result;
+    return result;
+  }
 
-buttons.addEventListener('click', e => {
+buttons.addEventListener('click', (e) => {
+    
     if (e.target.matches('.btn')) {
         const button = e.target;
-        action = button.dataset.action;
+        const action = button.dataset.action;
+        const keyContent = button.textContent;
+        const inScreenNum = up.textContent;
+        const previousKeyType = container.dataset.previousKeyType;
+
+        if (!action) {
+            if (inScreenNum === '0' || previousKeyType === 'operator') {
+                up.textContent = keyContent;
+            } else {
+                up.textContent = inScreenNum + keyContent;
+            }
+        }
+        
+        if (action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+          
+          container.dataset.previousKeyType = 'operator';
+          container.dataset.firstValue = inScreenNum;
+          container.dataset.operator = action;
+          console.log(container.dataset.operator);
+          
+        }
+        
+        if (action === 'decimal') {
+            up.textContent = inScreenNum + '.';
+          }
+          if (action === 'all-clear') {
+            up.textContent = '0';
+          }
+          
+        if (action === 'calculate') {
+            const firstValue = container.dataset.firstValue;
+            console.log(firstValue);
+            const operator = container.dataset.operator;
+            const secondValue = inScreenNum;
+            console.log(secondValue);
+            
+            up.textContent = calculate(firstValue, operator, secondValue);
+
+        }
+
+
         
     }
-    if (!action) {
-        console.log('number key!');
-    }
-    
-    if (action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide'
-    ) {
-      console.log('operator key');  
-    }
-    
-    if (action === 'decimal') {
-        console.log('decimal key!')
-      }
-      
-      if (action === 'all-clear') {
-        console.log('clear key!')
-      }
-      
-      if (action === 'calculate') {
-        console.log('equal key!')
-      }
 });
 
 
